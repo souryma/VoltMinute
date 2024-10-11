1a7adf91a431e8f0356feabd0fa4711682f59ab1
Antoine Martin <78165487+souryma@users.noreply.github.com>
2024-10-11 11:55:25 +0000

    Init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("⚡ VOLTMINUTE ⚡")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+st.write(
+    "PIKA PIKA"
 )
