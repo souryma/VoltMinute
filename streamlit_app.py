6c384d978057b50cfb4ff8d52e3d0b13eb9b2b19
souryma <antoine.martin885@gmail.com>
2024-11-19 10:53:01 +0100

    Added logo on main page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
+st.image("img/VoltMinuteLogo.png", caption="Logo")
+
 st.title("⚡ VOLTMINUTE ⚡")
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
