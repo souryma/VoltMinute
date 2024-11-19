d768ee1167416d0f911ccd5d3ba72daf8af11e10
Antoine Martin <78165487+souryma@users.noreply.github.com>
2024-11-19 10:19:01 +0000

    Added database connexion

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,38 @@
 import streamlit as st
 
-st.image("img/VoltMinuteLogo.png", caption="Logo")
+st.image("img/VoltMinuteLogo.png", width=146)
 
 st.title("⚡ VOLTMINUTE ⚡")
 
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Borne : **Champ de Mars (Angoulême)**"
 )
 st.write(
-    "PIKA PIKA"
+    "Batteries disponibles : "
 )
-st.text_input(
-    "Numero de batterie : "
-)
+
+from azure.cosmos import CosmosClient, exceptions
+
+# Configuration des informations de la base Cosmos DB
+secrets = st.secrets["cosmosdb"]
+COSMOS_URI = secrets["uri"]
+COSMOS_KEY = secrets["key"]
+DATABASE_NAME = secrets["database"]
+CONTAINER_NAME = secrets["container"]
+
+# Fonction pour se connecter à Cosmos DB
+@st.cache_resource
+def connect_to_cosmos_db():
+    try:
+        client = CosmosClient(COSMOS_URI, COSMOS_KEY)
+        database = client.get_database_client(DATABASE_NAME)
+        container = database.get_container_client(CONTAINER_NAME)
+        return container
+    except exceptions.CosmosHttpResponseError as e:
+        st.error(f"Erreur de connexion à Cosmos DB: {e}")
+        return None
+
+# Initialisation de la connexion
+container = connect_to_cosmos_db()
+if container:
+    st.success("Connexion à Cosmos DB établie avec succès !")
